ca47b8efe40ab89c456de9e54b6a91b6c26d0c7c
asadbek <asadbey0331@gmail.com>
2024-12-16 15:20:54 +0900

    :hammer: Update Docker configuration and entrypoint script; add Selenium dependency and improve startup process

--- redteam_core/miner/commits/webui_auto/docker-entrypoint.sh
+++ redteam_core/miner/commits/webui_auto/docker-entrypoint.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
-set -e
-
-# Activate conda environment
-# shellcheck disable=SC1091
-. /opt/conda/etc/profile.d/conda.sh
-conda activate base
-
-# Run the command passed to the container
-exec "$@"
+set -euo pipefail
+echo "INFO: Running 'webui-auto-miner' docker-entrypoint.sh..."
+_doStart()
+{
+	# exec python -u ./app.py || exit 2
+	exec uvicorn app:app --host=0.0.0.0 --port="${WUM_API_PORT:-10002}" --no-access-log --no-server-header --proxy-headers --forwarded-allow-ips='*' || exit 2
+	exit 0
+}
+main()
+{
+	umask 0002 || exit 2
+	sudo chown -Rc "${USER}:${GROUP}" "${WUM_HOME_DIR}" "${WUM_API_DATA_DIR}" "${WUM_API_LOGS_DIR}" "${WUM_API_TMP_DIR}" || exit 2
+	find "${WUM_API_DIR}" "${WUM_API_DATA_DIR}" -type d -exec chmod 770 {} + || exit 2
+	find "${WUM_API_DIR}" "${WUM_API_DATA_DIR}" -type f -exec chmod 660 {} + || exit 2
+	find "${WUM_API_DIR}" "${WUM_API_DATA_DIR}" -type d -exec chmod ug+s {} + || exit 2
+	find "${WUM_API_LOGS_DIR}" "${WUM_API_TMP_DIR}" -type d -exec chmod 775 {} + || exit 2
+	find "${WUM_API_LOGS_DIR}" "${WUM_API_TMP_DIR}" -type f -exec chmod 664 {} + || exit 2
+	find "${WUM_API_LOGS_DIR}" "${WUM_API_TMP_DIR}" -type d -exec chmod +s {} + || exit 2
+	chmod ug+x "${WUM_API_DIR}/app.py" || exit 2
+	# echo "${USER} ALL=(ALL) ALL" | sudo tee -a "/etc/sudoers.d/${USER}" > /dev/null || exit 2
+	echo ""
+	## Parsing input:
+	case ${1:-} in
+		"" | -s | --start | start | --run | run)
+			_doStart;;
+			# shift;;
+		-b | --bash | bash | /bin/bash)
+			shift
+			if [ -z "${*:-}" ]; then
+				echo "INFO: Starting bash..."
+				/bin/bash
+			else
+				echo "INFO: Executing command -> ${*}"
+				exec /bin/bash -c "${@}" || exit 2
+			fi
+			exit 0;;
+		*)
+			echo "ERROR: Failed to parsing input -> ${*}"
+			echo "USAGE: ${0}  -s, --start, start | -b, --bash, bash, /bin/bash"
+			exit 1;;
+	esac
+}
+main "${@:-}"
