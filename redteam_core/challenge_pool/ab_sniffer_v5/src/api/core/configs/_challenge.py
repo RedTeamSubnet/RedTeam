4c8285674ee743e757e2ce48cc8c3785f44b98e8
aliyuldashev <javohir.hbai@gmail.com>
2025-11-23 16:57:38 +0900

    refactor: removing unnecessary configurations

--- redteam_core/challenge_pool/ab_sniffer_v5/src/api/core/configs/_challenge.py
+++ redteam_core/challenge_pool/ab_sniffer_v5/src/api/core/configs/_challenge.py
@@ -15,13 +15,9 @@ class FrameworkImageConfig(BaseModel):
 class ChallengeConfig(FrozenBaseConfig):
     api_key: SecretStr = Field(..., min_length=12, max_length=128)
     docker_ulimit: int = Field(...)
-    # allowed_pip_pkg_dt: datetime = Field(...)
-    allowed_file_exts: List[str] = Field(..., min_length=1)
     bot_timeout: int = Field(..., ge=1)
-    framework_count: int = Field(..., ge=1)
     repeated_framework_count: int = Field(..., ge=1)
     framework_images: List[FrameworkImageConfig] = Field(...)
-    result_endpoint: AnyHttpUrl = Field(...)
 
     model_config = SettingsConfigDict(env_prefix=f"{ENV_PREFIX}CHALLENGE_")
 
