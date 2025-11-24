222e04c74edf7707150b63276d54fe0ef136256d
aliyuldashev <javohir.hbai@gmail.com>
2025-11-24 16:32:33 +0900

    feat: mount folder by run date

--- redteam_core/challenge_pool/controller.py
+++ redteam_core/challenge_pool/controller.py
@@ -196,6 +196,22 @@ class Controller(BaseController):
             allow_internet=False,
         )
 
+        # Set log volume with date path location for easy analysis
+        _challenge_run_kwargs = self.challenge_info.get(
+            "challenge_container_run_kwargs", {}
+        )
+        _challenge_volume = _challenge_run_kwargs.get("volumes", {})[0].split(":")
+        _volume_container_path = _challenge_volume[-1]
+        _volume_host_path = _challenge_volume[0]
+
+        _current_date = time.strftime("%Y-%m-%d")
+        _new_volume_host_path = (
+            f"{_volume_host_path}{_current_date}/logs/{self.challenge_name}"
+        )
+        _challenge_run_kwargs["volumes"] = [
+            f"{_new_volume_host_path}:{_volume_container_path}"
+        ] + _challenge_run_kwargs.get("volumes", [])[1:]
+
         # Run challenge container
         self.challenge_container = docker_utils.run_container(
             client=self.docker_client,
@@ -204,7 +220,7 @@ class Controller(BaseController):
             ports={
                 f"{constants.CHALLENGE_DOCKER_PORT}/tcp": constants.CHALLENGE_DOCKER_PORT
             },
-            **self.challenge_info.get("challenge_container_run_kwargs", {}),
+            **_challenge_run_kwargs,
         )
         bt.logging.info(
             f"[CONTROLLER] Challenge container started: {self.challenge_container.status}"
