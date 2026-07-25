ebb45af1f5b6a90dca583ae0d3b824cb70374453
aliyuldashev <javohir.hbai@gmail.com>
2026-07-26 07:52:35 +0900

    fix: update documentation links and clean up __all__ in __init__.py

--- src/redteam_core/__init__.py
+++ src/redteam_core/__init__.py
@@ -2,10 +2,4 @@ from .__version__ import __version__
 from .config import MainConfig, constants
 from .protocol import Commit
 
-__all__ = [
-    "__version__",
-    "Commit",
-    "MainConfig",
-    "constants",
-    "constant_docs",
-]
+__all__ = ["__version__", "Commit", "MainConfig", "constants"]
